304e27716a4a095462932a72526e04d68db6145c
Vitus Lehner <>
2019-08-20 17:12:11 +0200

    Minor formatting

--- src/utils/logging.tsx
+++ src/utils/logging.tsx
@@ -1,13 +1,13 @@
 const loggingPrefix = "[AQWidget]";
 
-export function error(...args: any[]){
-    console.error(loggingPrefix, ...args)
+export function error(...args: any[]) {
+    console.error(loggingPrefix, ...args);
 }
 
-export function warn(...args: any[]){
-    console.warn(loggingPrefix, ...args)
+export function warn(...args: any[]) {
+    console.warn(loggingPrefix, ...args);
 }
 
-export function info(...args: any[]){
-    console.info(loggingPrefix, ...args)
+export function info(...args: any[]) {
+    console.info(loggingPrefix, ...args);
 }
